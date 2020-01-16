c2c651ccad1306f6c397f83aeb799ef8fee1f08d
koassumo <0770407@gmail.com>
2020-01-16 17:53:50 +0300

    сделан урок 4

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,27 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea; // внимание! иногда цепляется не та библиотека - AWT
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    TextArea mainArea;
+
+    @FXML
+    TextField msgField;
+
+
+    public void sendMsgAction(ActionEvent actionEvent) {
+        mainArea.appendText(msgField.getText() + "\n");
+        msgField.clear();
+        msgField.requestFocus();
+    }
+
+    public void sendMsg(String textMsg) {
+        mainArea.appendText(textMsg + "\n");
+        msgField.requestFocus();
+    }
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,13 +11,23 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Client_FX");
+        primaryStage.setScene(new Scene(root, 500, 500));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
+
+        // javaFX
         launch(args);
+//        как отправить текст в окно java fx ??? ((
+//        Controller controller = new Controller();
+//        controller.sendMsg("тест отправки текста из мейна");
+
+
+        // Swing
+        GUI_Swing gui_Swing = new GUI_Swing();
+        gui_Swing.setNewText("тест отправки текста из мейна");
     }
 }
